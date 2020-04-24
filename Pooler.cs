eb6600eb04c937d623735ed460d0bcc3cada8dd3
eduardo1995filipe <eduardo_95@yahoo.com>
2020-04-24 23:08:31 +0100

    1. First Commit
    2. Initial working state

---
+++ Pooler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pooler : MonoBehaviour
+{
+
+    public GameObject poolingPrefab;
+
+    public int initialPoolingAmount;
+    public int maxAmount;
+
+    private List<GameObject> pooledObjects;
+
+    public GameObject Get()
+    {
+        return null;
+    }
+
+    public GameObject Eat()
+    {
+        return null;
+    }
+}
